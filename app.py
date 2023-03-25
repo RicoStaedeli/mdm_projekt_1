b44206e39b4be33a77a1e51290169ea5bc15e2b9
Rico Staedeli ZHAW <staedric@students.zhaw.ch>
2023-03-25 17:13:50 +0100

    add model to deploy

--- app.py
+++ app.py
@@ -17,28 +17,21 @@ def indexPage():
 @app.route('/upload', methods=['POST'])
 def upload_image():
     
-
     # Get the uploaded image from the request
     image_file = request.files['image']
-    
-    
 
+    from PIL import Image
+    from transformers import AutoProcessor, AutoModelForCausalLM
+    image = Image.open(image_file)
+    processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco") 
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    # from PIL import Image
-    # from transformers import AutoProcessor, AutoModelForCausalLM
-    # image = Image.open(image_file)
-    # processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
-    # model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco") 
-    # pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    # generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-    # generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-    generated_caption ="This is fkn nonsense"
+    # generated_caption ="This is fkn nonsense"
     print(generated_caption)
        
-    # Save the image to disk (optional)
-    #image_file.save('uploaded_image.jpg')
-
     return jsonify({
         "generated_caption": generated_caption,
     })
