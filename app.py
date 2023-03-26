1777ac838efc8c36e58962b00294cbd57b3ae367
Rico Staedeli ZHAW <staedric@students.zhaw.ch>
2023-03-26 11:17:24 +0200

    disabel model

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ def upload_image():
     # Get the uploaded image from the request
     image_file = request.files['image']
 
-    from PIL import Image
-    from transformers import AutoProcessor, AutoModelForCausalLM
-    image = Image.open(image_file)
-    processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
-    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco") 
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
-    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-    generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    #from PIL import Image
+    #from transformers import AutoProcessor, AutoModelForCausalLM
+    #image = Image.open(image_file)
+    #processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+    #model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco") 
+    #pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    #generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+    #generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    # generated_caption ="This is fkn nonsense"
+    generated_caption ="This is fkn nonsense"
     print(generated_caption)
        
     return jsonify({
