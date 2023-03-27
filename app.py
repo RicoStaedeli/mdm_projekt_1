98182454b2516358e1031cd65e51020ca9639df4
RicoStaedeli <62505224+RicoStaedeli@users.noreply.github.com>
2023-03-27 17:58:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 from flask.helpers import send_file
 # from PIL import Image
 # from transformers import AutoProcessor, AutoModelForCausalLM
-from transformers import SegformerFeatureExtractor, SegformerForImageClassification
-from PIL import Image
 
 app = Flask(__name__, static_url_path='/', static_folder='web')
 
@@ -18,24 +16,14 @@ def upload_image():
     # Get the uploaded image from the request
     image_file = request.files['image']
 
-
     image = Image.open(image_file)
     # processor = AutoProcessor.from_pretrained("microsoft/git-base")
     # model = AutoModelForCausalLM.from_pretrained("microsoft/git-base") 
     # pixel_values = processor(images=image, return_tensors="pt").pixel_values
     # generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
     # generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/mit-b0")
-    model = SegformerForImageClassification.from_pretrained("nvidia/mit-b0")
 
-    inputs = feature_extractor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits
-    # model predicts one of the 1000 ImageNet classes
-    predicted_class_idx = logits.argmax(-1).item()
-    generated_caption = model.config.id2label[predicted_class_idx]
-
-    #generated_caption ="This is fkn nonsense"
+    generated_caption ="This is fkn nonsense"
     print(generated_caption)
        
     return jsonify({
